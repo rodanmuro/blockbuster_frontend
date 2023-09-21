b36f2821567bfa9a001fc542b730760a363ca3c1
rodanmuro <rodanmuro@gmail.com>
2023-09-21 15:45:41 -0500

    Revert "video muy largo"
    This reverts commit 83cbfd8596320cb2beabb8ae8c1c93eb1f1d3c50.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,65 @@
 import React, { useContext, useState } from 'react'
 import { getJwt, login } from '../utils/apiFunctions';
 import { useNavigate } from 'react-router';
-import LoginRegister from '../components/loginregister/LoginRegister';
 
-
+import { loggedContext, userContext } from '../App';
+import jwtDecode from 'jwt-decode';
 
 const Login = () => {
 
+  const navigate = useNavigate();
+  const {logged, setLogged} = useContext(loggedContext);
+  const {user, setUser} = useContext(userContext);
+
+  const[username, setUsername] =useState('');
+  const[password, setPassword] =useState('');
+
+  const handleChangeUsername= (event)=>{
+    setUsername(event.target.value)
+  }
+
+  const handleChangePassword= (event)=>{
+    setPassword(event.target.value)
+  }
+
+  const loguear = async ()=>{
+    let mensaje = await login(username, password);
+    if(mensaje=="success"){
+      setLogged(true);
+
+      if(getJwt()){
+        const data = jwtDecode(getJwt());
   
+        setUser(
+          {
+            username:data.username,
+            role:data.role
+          }
+        );
+      }
+
+      navigate("/");
+    }else{
+      console.log(mensaje);
+    }
+  }
 
   return (
     <div>
-      <LoginRegister/>
+      <div>
+        <label>Username</label>
+        <input type="text" onChange={handleChangeUsername} />
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input type="password" onChange={handleChangePassword}/>
+      </div>
+
+      <div>
+        <button onClick={loguear}>Ingresar</button>
+      </div>
+
     </div>
   )
 }
