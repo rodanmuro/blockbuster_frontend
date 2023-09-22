f40a2b8494e8deb1becf33f984a9b6b07d72f8c8
rodanmuro <rodanmuro@gmail.com>
2023-09-21 19:09:44 -0500

    restaurando al video 38 modificando el backend para agregar jwt con el fin de volver a hacer el loginregister

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -2,7 +2,7 @@ const API_BASE_URL = "http://localhost:8080"
 
 const API_ENDPOINTS = {
     login: `${API_BASE_URL}/login`,
-    registro: `${API_BASE_URL}/usuario`,
+    usuario: `${API_BASE_URL}/usuario`,
     peliculacatalogo: `${API_BASE_URL}/peliculacatalogo`,
 }
 
