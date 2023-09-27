7fb4507ee79e2bea19119af6b5142b2807e949ba
rodanmuro <rodanmuro@gmail.com>
2023-09-26 23:35:24 -0500

    Revert "se creo obtenerpeliculas en apifunction se creo el endpoint urlimage en catalogo se agrego la cardmovie y cardmovie css"
    This reverts commit 0cb1287ea0a5962d9552cc96a7d1e8467f18aefa.

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,50 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 
 import { userContext } from '../App'
-import { obtenerPeliculasCatalogo } from '../utils/apiFunctions';
-import CardMovie from '../components/cardmovie/CardMovie';
 
 const Catalogo = () => {
 
-  const { user } = useContext(userContext);
-
-  const [movies, setMovies] = useState([]);
-
-  const cargarPeliculas = async () => {
-    let movies_ = await obtenerPeliculasCatalogo();
-    if (!movies_.error) {
-      setMovies(movies_)
-    }
-  }
-
-  useEffect(
-    () => {
-      cargarPeliculas();
-    }
-    ,
-    []
-  );
+const {user} = useContext(userContext);
 
   return (
-    <>
-      <div className='row'>
-        <div className='col-12'>
-          <h2 className='text-center py-5'>
-            Catálogo de Películas
-          </h2>
-        </div>
-      </div>
-
-      <div className='container'>
-        <div className="row pt-3 d-flex justify-content-center">
-          {movies.map(
-            (movie) => (
-              <CardMovie movie={movie} />
-            )
-          )}
-        </div>
-      </div>
-    </>
+    <div>Catalogo Hola {user.username} con role {user.role}</div>
   )
 }
 
--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -4,7 +4,6 @@ const API_ENDPOINTS = {
     login: `${API_BASE_URL}/login`,
     registro: `${API_BASE_URL}/usuario`,
     peliculacatalogo: `${API_BASE_URL}/peliculacatalogo`,
-    urlImage: "https://image.tmdb.org/t/p/original"
 }
 
 export {API_ENDPOINTS}
